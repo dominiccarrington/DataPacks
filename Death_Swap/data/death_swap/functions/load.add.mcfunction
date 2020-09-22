198dcb6e1c035c59ef6d04da8251b44ab6181d42
Dominic Carrington <carringtondominic@gmail.com>
2020-09-22 23:30:50 +0100

    Adapted for tellraw and fixed a few bugs

---
+++ Death_Swap/data/death_swap/functions/load.add.mcfunction
@@ -0,0 +1,24 @@
+####
+# Death Swap
+# Game Originaly By Sethbling
+# Datapack By RageGamingPE
+####
+
+gamerule commandBlockOutput false
+
+scoreboard objectives add player dummy
+scoreboard objectives add _player dummy
+scoreboard objectives add timer dummy
+scoreboard objectives add health health
+scoreboard objectives add deaths deathCount
+scoreboard objectives setdisplay list health
+
+scoreboard players set @a player 0
+execute as @a run scoreboard players add #players player 1
+scoreboard players set #default timer 1200
+scoreboard players operation #default timer /= #players player
+scoreboard players operation @a timer = #default timer
+
+kill @e[type=armor_stand,name=DeathSwap]
+
+tellraw @a {%load%}
--- build.js
+++ build.js
@@ -7,18 +7,31 @@ glob("**/functions/*.add.mcfunction", function (err, files) {
     const replacementRegExp = /\{\%(\w+)\%\}/;
 
     for (let file of files) {
-        let content = fs.readFileSync(file).toString();
+        let fileContent = fs.readFileSync(file).toString();
+        let lines = fileContent.split("\n");
 
-        let match;
-        while (match = replacementRegExp.exec(content)) {
-            content = content.replace(match[0], convert(path.dirname(path.dirname(file)) + "/books/" + match[1] + ".mdf"));
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+
+            let match;
+            if (match = replacementRegExp.exec(line)) {
+                const content = path.dirname(path.dirname(file)) + "/content/" + match[1] + ".mdf";
+
+                if (line.startsWith('give')) {
+                    line = line.replace(match[0], convertForBook(content));
+                } else if (line.startsWith('tellraw')) {
+                    line = convertForTellraw(content, line.slice(0, match.index));
+                }
+            }
+
+            lines[i] = line;
         }
 
-        fs.writeFileSync(file.replace(".add", ""), content);
+        fs.writeFileSync(file.replace(".add", ""), lines.join("\n"));
     }
 });
 
-function convert(file) {
+function convertForBook(file) {
     let output = {pages:[]};
 
     const content = fs.readFileSync(file).toString();
@@ -56,134 +69,154 @@ function convert(file) {
             line = lines[++i];
         }
 
-        let status = {
-            bold: false,
-            italic: false,
-            underline: false,
-            strikethrough: false,
-            obfuscated: false,
-            color: 'reset'
-        };
-
-        let text = "";
-        let prevChar = "";
-        const lookoutFor = ['*', '_', '-', '#', '%', '[', '$'];
-        for (let j = 0; j < line.length; j++) {
-            const char = line[j];
-            if (prevChar !== "\\" && lookoutFor.includes(char)) {
-                if (text) {
-                    page.push({text, ...status});
-                    text = "";
-                }
-
-                if (char == "*" && line[j+1] == "*") {
-                    status.bold = !status.bold;
-                    j++;
-                } else if (char == "*") {
-                    status.italic = !status.italic;
-                } else if (char == "_") {
-                    status.underline = !status.underline;
-                } else if (char == "-") {
-                    status.strikethrough = !status.strikethrough;
-                } else if (char == "#") {
-                    status.obfuscated = !status.obfuscated;
-                } else if (char == "%") {
-                    if (line[j+1] == "(" && status.color == 'reset') {
-                        let skip = 2;
-                        status.color = "#";
-                        while (line[j+skip] !== ")") {
-                            status.color += line[j+skip];
-                            skip++;
-                        }
-                        j += skip;
-                    } else {
-                        status.color = "reset";
-                    }
-                } else if (char == "[") {
-                    let skip = 1;
-                    while (line[j+skip] !== "]") {
-                        text += line[j+skip];
-                        skip++;
-                    }
-                    skip++;
-
-                    // '(' means click action
-                    let click = "";
-                    if (line[j+skip] == "(") {
-                        skip++;
-                        while (line[j+skip] !== ")") {
-                            click += line[j+skip];
-                            skip++;
-                        }
-                        skip++;
-                    }
-                    
-                    // '{' means hover
-                    let hover = "";
-                    if (line[j+skip] == "{") {
-                        skip++;
-                        while (line[j+skip] !== "}") {
-                            hover += line[j+skip];
-                            skip++;
-                        }
-                        skip++;
-                    }
-
-                    let out = {text, ...status};
-
-                    if (click) {
-                        const urlRegEx = new RegExp('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
-                        let action = "";
-
-                        if (!isNaN(parseInt(click))) {
-                            action = "change_page";
-                            click = parseInt(click);
-                        } else if (urlRegEx.test(click)) {
-                            action = "open_url";
-                        } else  {
-                            action = "run_command";
-                        }
-
-                        out["clickEvent"] = {"action":action,"value":click};
-                    }
-
-                    if (hover) out["hoverEvent"] = {"action":"show_text","contents":hover};
-
-                    page.push(out);
-                    text = "";
-                    j += skip;
-                } else if (char == "$" && line[j+1] == "{") {
-                    let skip = 2;
-                    let player = "";
-                    let objective = "";
-                    while (line[j+skip] !== "}") {
-                        player += line[j+skip];
-                        skip++;
-                    }
-
-                    if (player.includes(".")) {
-                        [player, objective] = player.split(".");
-                    }
-                    
-                    if (objective) {
-                        page.push({"score": {"name": player, "objective": objective}, ...status});
-                    } else {
-                        page.push({"selector":"@p", ...status});
-                    }
-                    j += skip;
-                }
-            } else {
-                text += char;
-                prevChar = char;
-            }
-        }
+        page = convertLine(line, page, "reset");
 
         line = lines[++i];
-        if (text) page.push({text, 'color': 'reset'});
         page.push({"text": '\n', 'color': 'reset'});
     }
 
     output['pages'].push(JSON.stringify(page));
 
     return JSON.stringify(output)
+}
+
+function convertForTellraw(file, start) {
+    const content = fs.readFileSync(file).toString();
+    const lines = content.split("\n");
+
+    let output = [];
+    for (let line of lines) {
+        output.push(convertLine(line, [], "white"));
+    }
+
+    return output.map((el) => start + JSON.stringify(el)).join("\n");
+}
+
+function convertLine(line, page, defaultColor) {
+    let status = {
+        bold: false,
+        italic: false,
+        underline: false,
+        strikethrough: false,
+        obfuscated: false,
+        color: defaultColor
+    };
+
+    let text = "";
+    const lookoutFor = ['*', '_', '-', '#', '%', '[', '$'];
+    for (let j = 0; j < line.length; j++) {
+        const char = line[j];
+        if (char === "\\") {
+            text += line[++j];
+        } else if (lookoutFor.includes(char)) {
+            if (text) {
+                page.push({text, ...status});
+                text = "";
+            }
+
+            if (char == "*" && line[j+1] == "*") {
+                status.bold = !status.bold;
+                j++;
+            } else if (char == "*") {
+                status.italic = !status.italic;
+            } else if (char == "_") {
+                status.underline = !status.underline;
+            } else if (char == "-") {
+                status.strikethrough = !status.strikethrough;
+            } else if (char == "#") {
+                status.obfuscated = !status.obfuscated;
+            } else if (char == "%") {
+                if (line[j+1] == "(" && status.color == defaultColor) {
+                    let skip = 2;
+                    status.color = line[j+skip] === line[j+skip].toUpperCase() ? "#" : "";
+                    while (line[j+skip] !== ")") {
+                        status.color += line[j+skip];
+                        skip++;
+                    }
+                    j += skip;
+                } else {
+                    status.color = defaultColor;
+                }
+            } else if (char == "[") {
+                [text, j] = absorbTextUntil(line, j+1, "]");
+                j++;
+
+                // '(' means click action
+                let click = "";
+                if (line[j] == "(") {
+                    [click, j] = absorbTextUntil(line, j+1, ")");
+                    j++;
+                }
+                
+                // '{' means hover
+                let hover = "";
+                if (line[j] == "{") {
+                    [hover, j] = absorbTextUntil(line, j+1, "}");
+                    j++;
+                }
+
+                let out = {text, ...status};
+
+                if (click) {
+                    let action = "";
+
+                    if (!isNaN(parseInt(click))) {
+                        action = "change_page";
+                        click = parseInt(click);
+                    } else if (click.startsWith("/")) {
+                        action = "run_command";
+                    } else  {
+                        action = "open_url";
+                    }
+
+                    out["clickEvent"] = {"action":action,"value":click};
+                }
+
+                if (hover) out["hoverEvent"] = {"action":"show_text","contents":hover};
+                page.push(out);
+                text = "";
+            } else if (char == "$" && line[j+1] == "{") {
+                let skip = 2;
+                let player = "";
+                let objective = "";
+                while (line[j+skip] !== "}") {
+                    player += line[j+skip];
+                    skip++;
+                }
+
+                if (player.includes(".")) {
+                    [player, objective] = player.split(".");
+                }
+                
+                if (objective) {
+                    page.push({"score": {"name": player, "objective": objective}, ...status});
+                } else {
+                    page.push({"selector":"@p", ...status});
+                }
+                j += skip;
+            }
+        } else {
+            text += char;
+        }
+    }
+
+    if (text) page.push({text, 'color': defaultColor});
+
+    return page;
+}
+
+function absorbTextUntil(line, start, until) {
+    let skip = 0;
+    let text = "";
+    let char;
+
+    while ((char = line[start+skip]) !== until) {
+        if (char === "\\") {
+            text += line[start+(++skip)];
+        } else {
+            text += char;
+        }
+        skip++;
+    }
+    return [text, start+skip];
 }
